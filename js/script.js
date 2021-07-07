75847ce9cc6d3a56a82efc93fa54f5cb6a70f430
sr13579 <sohanarafat703@gmail.com>
2021-07-07 10:41:42 +0600

    Add files via upload
    Some small fixes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 $(document).ready(()=>{//This is the starting line of reasy function
-    
     $("#send").click(()=>{
+        var meth=$("#method").val();
         $.ajax({
             url:$("#url").val(),
-            success:(data,status,statcode)=>{$("#stat").html(statcode.status);
-            $("#stat").html(data);
-            console.log(statcode.status)}    
+            method:meth,
+            success:(data,status,statcode)=>{
+                $("#stat").html(statcode.status);
+                $("#output").html(data);
+            },
+            error:(xhr,status,error)=>{
+                $("#stat").html(xhr.status);
+                $("#output").html(status);
+            },    
         })
     });
-
 });//This is the last line of ready function
